d9a25071aa5d1b1382426815731384be64404beb
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-02-05 10:33:05 +0000

    patch alpha

--- config.def.h
+++ config.def.h
@@ -101,6 +101,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.97;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
